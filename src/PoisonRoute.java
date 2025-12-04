86e150811cbe50e93008d835920bed73ba56f3bf
JavierClemente <javierclementejr@gmial.com>
2025-12-04 10:02:54 +0100

    upd

--- src/PoisonRoute.java
+++ src/PoisonRoute.java
@@ -3,34 +3,31 @@ import java.util.Scanner;
 
 public class PoisonRoute {
 
-    static String nombrePokemon = "";
-
-    static int vidaSeleccionada = 0;
-
-    static int vidaPokemon = 0;
-
-    static void inicioPoisonRoute() {
-        main(null);
-    }
+    static Scanner leer = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        int vidas = 3;
+        inicioPoisonRoute();
+    }
 
+    static void inicioPoisonRoute() {
         System.out.println();
         Metodos.slowPrintln("BIENVENIDO AL CAMINO DE LUZ DISTORSIONADA", 30);
         System.out.println();
         Metodos.slowPrintln("Te encuentras en un bosque de espejos engañosos, tienes que resolver un ahorcado para avanzar.", 20);
         Metodos.slowPrintln("Cada vez que te equivocas en una letra la vida de tu pokemon disminuye, y si muere tu pokemon comienzas de 0 el ahorcado.", 20);
-        Metodos.slowPrintln("Tienes " + vidas + " vidas. Si te quedas sin vidas pierdes.", 20);
+        Metodos.slowPrintln("Tienes 3 vidas. Si te quedas sin vidas pierdes.", 20);
         System.out.println();
         Metodos.slowPrintln("QUE COMIENCE EL JUEGO", 20);
 
-        ahorcado(nombrePokemon, vidas, vidaPokemon);
+        ahorcado();
     }
 
-    static void ahorcado(String pokemon, int vidas,  int vidaPokemon) {
-        Scanner leer = new Scanner(System.in);
+    static void ahorcado() {
+        int vidas = 3;
+        DatosHistoria.vidaInicial = DatosHistoria.vidaActual;
+        int vidaPokemon = DatosHistoria.vidaActual;
+        String pokemon = DatosHistoria.Pokemon;
+        System.out.println(vidaPokemon+ " y " + pokemon);
         boolean adivinado = false;
         Random aleatorio = new Random();
 
@@ -42,10 +39,7 @@ public class PoisonRoute {
         };
 
         String palabra = box_ahorcado_palabra[aleatorio.nextInt(box_ahorcado_palabra.length)];
-
         char[] palabraOculta = new char[palabra.length()];
-
-        Metodos.slowPrintln("ADIVINA LA PALABRA", 30);
 
         for (int i = 0; i < palabraOculta.length; i++) {
             palabraOculta[i] = '_';
@@ -58,62 +52,35 @@ public class PoisonRoute {
 
         String letrasFalladas = "";
 
-        while (vidaPokemon > 0 && !adivinado) {
-
+        while (vidaPokemon > 0 && vidas > 0 && !adivinado) {
             boolean acierto = false;
             System.out.println();
+            System.out.println("Palabra: " + String.valueOf(palabraOculta));
+            System.out.println("\u001B[32mVida del pokemon: " + vidaPokemon + ". Vidas restantes: " + vidas + "\u001B[0m");
+            System.out.println();
+            System.out.println("Que quieres hacer:");
+            System.out.println("1: Adivinar palabra");
+            System.out.println("2: Adivinar letra");
+            System.out.print("Opcion: ");
 
             int elegir;
-
             while (true) {
-                System.out.println("Palabra: " + String.valueOf(palabraOculta));
-                System.out.println("\u001B[32mVida del pokemon: " + vidaPokemon + "." + " Vidas restantes: " + vidas + "\u001B[0m");
-                System.out.println();
-                System.out.println("Que quieres hacer:");
-                System.out.println("1: Adivinar palabra");
-                System.out.println("2: Adivinar letra");
-                System.out.print("Opcion: ");
-
                 if (leer.hasNextInt()) {
                     elegir = leer.nextInt();
-                    if (elegir == 1 ||  elegir == 2) {
-                        break;
-                    } else  {
-                        System.out.println();
-                        System.out.println("\u001B[31m¡ERROR! Solo puedes introducir 1 o 2\u001B[0m");
-                    }
-
+                    if (elegir == 1 || elegir == 2) break;
+                    else System.out.println("\u001B[31m¡ERROR! Solo puedes introducir 1 o 2\u001B[0m");
                 } else {
-                    System.out.println();
                     System.out.println("\u001B[31m¡ERROR! Solo puedes introducir numeros\u001B[0m");
                     leer.next();
                 }
-
-
             }
 
             switch (elegir) {
                 case 1:
-                    System.out.println();
-                    System.out.println("Palabra: " + String.valueOf(palabraOculta));
-                    System.out.println();
                     System.out.println("Ingresa la palabra: ");
+                    String adivinarPalabra = leer.next().toLowerCase();
 
-                    String adivinarPalabra = leer.next();
-
-                    adivinarPalabra = adivinarPalabra.toLowerCase();
-
-                    boolean soloLetras = true;
-
-                    for (int i = 0; i < adivinarPalabra.length(); i++) {
-                        if (!Character.isLetter(adivinarPalabra.charAt(i))) {
-                            soloLetras = false;
-                            break;
-                        }
-                    }
-
-                    if (!soloLetras) {
-                        System.out.println();
+                    if (!adivinarPalabra.matches("[a-zA-Z]+")) {
                         System.out.println("\u001B[31m¡ERROR! Solo se admiten letras\u001B[0m");
                         break;
                     }
@@ -121,31 +88,33 @@ public class PoisonRoute {
                     if (adivinarPalabra.equals(palabra)) {
                         adivinado = true;
                     } else {
-                        System.out.println();
                         System.out.println("\u001B[31mINCORRECTO\u001B[0m");
-                        vidaPokemon -= 20;
-                        break;
-                    }
-                    if (vidaPokemon <= 0) {
-                        reinicio(vidas, vidaPokemon, pokemon);
+                        DatosHistoria.vidaActual -= 20;
+                        if (vidaPokemon <= 0) {
+                            vidas--;
+                            if (vidas > 0) {
+                                System.out.println("\u001B[31mPS DE TU POKEMON AGOTADOS\u001B[0m");
+                                DatosHistoria.vidaActual = DatosHistoria.vidaInicial;
+                                palabraOculta = new char[palabra.length()];
+                                for (int i = 0; i < palabraOculta.length; i++) palabraOculta[i] = '_';
+                            }
+                        }
                     }
                     break;
 
-
                 case 2:
-                    System.out.println();
-                    System.out.println("Palabra: " + String.valueOf(palabraOculta));
-                    System.out.println("Letras falladas: " + letrasFalladas);
                     System.out.print("Ingresa una letra: ");
-
-                    String unaLetra = leer.next();
-
-                    if (unaLetra.length() != 1) {
-                        System.out.println("\n\u001B[31m¡ERROR! Solo se permite ingresar una letra a la vez.\u001B[0m");
+                    String unaLetra = leer.next().toLowerCase();
+                    if (unaLetra.length() != 1 || !unaLetra.matches("[a-z]")) {
+                        System.out.println("\u001B[31m¡ERROR! Solo se permite ingresar una letra valida\u001B[0m");
                         break;
                     }
 
-                    char letra = Character.toLowerCase(unaLetra.charAt(0));
+                    char letra = unaLetra.charAt(0);
+                    if (letrasFalladas.contains(String.valueOf(letra)) || String.valueOf(palabraOculta).contains("" + letra)) {
+                        System.out.println("\u001B[31mYa has intentado esta letra\u001B[0m");
+                        break;
+                    }
 
                     for (int i = 0; i < palabra.length(); i++) {
                         if (palabra.charAt(i) == letra && palabraOculta[i] == '_') {
@@ -154,50 +123,41 @@ public class PoisonRoute {
                         }
                     }
 
-                    if (!Character.isLetter(letra)) {
-                        System.out.println();
-                        System.out.println("\u001B[31m¡ERROR! Solo se admiten letras\u001B[0m");
-                        break;
-                    }
-
                     if (!acierto) {
-                        System.out.println(" ");
-                        vidaPokemon -= 5;
                         System.out.println("\u001B[31mEsta letra no esta en la palabra\u001B[0m");
-
-                        if (!letrasFalladas.contains(String.valueOf(letra))) {
-                            letrasFalladas += letra + " ";
-                        }
+                        DatosHistoria.vidaActual -= 5;
+                        vidaPokemon = DatosHistoria.vidaActual;
+                        letrasFalladas += letra + " ";
                         if (vidaPokemon <= 0) {
-                            reinicio(vidas, vidaPokemon, pokemon);
-                            break;
+                            vidas--;
+                            if (vidas > 0) {
+                                System.out.println("\u001B[31mTU POKEMON MURIO, reiniciando ahorcado\u001B[0m");
+                                vidaPokemon = DatosHistoria.vidaInicial;
+                                palabraOculta = new char[palabra.length()];
+                                for (int i = 0; i < palabraOculta.length; i++) palabraOculta[i] = '_';
+                                letrasFalladas = "";
+                            }
                         }
                     } else {
-                        System.out.println(" ");
                         System.out.println("\u001B[32mLETRA CORRECTA\u001B[0m");
                     }
 
-                    if (String.valueOf(palabraOculta).equals(palabra)) {
-                        adivinado = true;
-                    }
+                    if (String.valueOf(palabraOculta).equals(palabra)) adivinado = true;
                     break;
             }
         }
 
         if (adivinado) {
-            System.out.println(" ");
-            System.out.println("\u001B[32mFELICIDADES ADIVINASTE LA PALABRA");
-            System.out.println("GANASTE EL JUEGO\u001B[0m");
+            System.out.println("\u001B[32mFELICIDADES ADIVINASTE LA PALABRA. GANASTE EL JUEGO\u001B[0m");
             recompensa(vidaPokemon);
-
         } else {
-            reinicio(vidas, vidaPokemon, pokemon);
+            System.out.println("\u001B[31mHAS PERDIDO TODAS LAS VIDAS\u001B[0m");
         }
     }
 
     static void ayudaLetras(String palabra, char[] palabraOculta, double porcentaje) {
         Random aleatorio = new Random();
-        int letrasARevelar = (int) (palabra.length() * porcentaje);
+        int letrasARevelar = Math.max(1, (int) (palabra.length() * porcentaje));
         int reveladas = 0;
         while (reveladas < letrasARevelar) {
             int pos = aleatorio.nextInt(palabra.length());
@@ -208,66 +168,19 @@ public class PoisonRoute {
         }
     }
 
-    static int reinicio (int vidas, int vidaPokemon, String pokemon){
-        Scanner leer = new Scanner(System.in);
-        if (vidas <= 0){
-            System.out.println(" ");
-            Metodos.slowPrintln("\u001B[31mHAS PERIDIDO" + " NO TE QUEDAN MAS VIDAS", 20);
-            int opcion = 0;
-            while (true){
-                System.out.println(" ");
-                System.out.println("Que quieres hacer: ");
-                System.out.println("1: Volver a intentarlo");
-                System.out.println("2: Salir del juego");
-                System.out.print("Opcion:\u001B[0m");
-
-                if (leer.hasNextInt()) {
-                    opcion = leer.nextInt();
-                    break;
-                }else {
-                    System.out.println("\u001B[31m¡ERROR! Solo puedes introducir numeros\u001B[0m");
-                    leer.next();
-                }
-            }
-            switch (opcion) {
-                case 1:
-                    main(null);
-                    return 3;
-                case 2:
-                    System.exit(0);
-                    break;
-            }
-        }
-        if (vidaPokemon <= 0){
-            System.out.println();
-            vidas--;
-            vidaPokemon = vidaSeleccionada;
-            Metodos.slowPrintln("\u001B[31mHAS PERIDIDO" + " TU POKEMON NO TIENE VIDA", 20);
-            Metodos.slowPrintln("VUELVE A INTENTARLO\u001B[0m", 20);
-
-            ahorcado(pokemon,vidas,vidaPokemon);
-        }
-        return vidas;
-    }
-
-    static String recompensa(int vidaPokemon){
-        Scanner leer = new Scanner(System.in);
-        String abrir;
-        Metodos.slowPrintln("Ahora podras abrir este cofre, solo tienes que escribir 'Abrir' para abrirlo: ",20);
-        abrir = leer.next();
-
-        while (!abrir.equalsIgnoreCase("Abrir")){
+    static void recompensa(int vidaPokemon) {
+        System.out.println("Ahora puedes abrir el cofre, escribe 'Abrir' para abrirlo:");
+        String abrir = leer.next();
+        while (!abrir.equalsIgnoreCase("Abrir")) {
             System.out.println("Solo puedes poner 'Abrir' para abrirlo");
             abrir = leer.next();
         }
         recompensaAleatorio(vidaPokemon);
-        return abrir;
     }
 
-    static void recompensaAleatorio(int vidaPokemon){
+    static void recompensaAleatorio(int vidaPokemon) {
         Random aleatorio = new Random();
         boolean pokeballAleatoria = aleatorio.nextBoolean();
-
         String amuleto = "Amuleto de protección";
         String piedraEvolucion = "Piedra Lunar";
         String pokeball = "Pokeball";
@@ -278,7 +191,5 @@ public class PoisonRoute {
             System.out.println("Objetos conseguidos: " + amuleto + " y " + piedraEvolucion);
         }
         System.out.println("Tu pokemon tiene "+vidaPokemon+" de vida");
-
     }
 }
-
